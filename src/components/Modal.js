29cb3fb91c803652765e771adf49977c2d1ab7b1
Lester Yiu <lester.ws.yiu@gmail.com>
2022-07-14 05:53:40 -0400

    fixed modal issue that shows previous answer on last question

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,24 @@
-const Modal = (props) => {
-    const {setIsModalOn, resultsData, currentQuestion, userAnswer, decodeText } = props;
+import { useEffect, useState } from "react";
 
+const Modal = (props) => {
+    const {setIsModalOn, resultsData, userAnswer, decodeText, currentQuestion, modalCounter, setModalCounter } = props;
+
+    const [modalQuestionNum, setModalQuestionNum] = useState(0);
+    
     const handleClick = () => {
         setIsModalOn(false);
     }
+    
+    useEffect( () => {
+        if (resultsData.gameData.length - 1 > currentQuestion) {
+            setModalQuestionNum(currentQuestion - 1);
+        } else if (resultsData.gameData.length - 1 === currentQuestion && modalCounter === 1) {
+            setModalQuestionNum(resultsData.gameData.length - 1);
+        } else if (resultsData.gameData.length - 1 === currentQuestion) {
+            setModalCounter(modalCounter + 1);
+            setModalQuestionNum(currentQuestion - 1);
+        }
+    }, [resultsData.gameData.length, currentQuestion, modalCounter, setModalCounter])
 
     return(
         <div className='modal'>  
@@ -16,7 +31,7 @@ const Modal = (props) => {
                 </div>
                 <h3>Nice Try!</h3>
                 <div className='modalText'>
-                    <div><span className='correct-answer'>Correct Answer:</span> <p>{decodeText(resultsData.gameData[currentQuestion - 1].correct_answer)}</p></div>
+                    <div><span className='correct-answer'>Correct Answer:</span> <p>{decodeText(resultsData.gameData[modalQuestionNum].correct_answer)}</p></div>
                     <div className='yourAnswerParagraph'><span className='yourAnswer'>Your Answer:</span><p>{decodeText(userAnswer)}</p></div>
                 </div>
                 <button onClick={handleClick} className='exitModal currentGameButton'>Continue</button>
